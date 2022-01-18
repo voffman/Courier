e3a4cfc3f4723626de0a2a9171a49495a729d0b1
Vladimir <vovker27@gmail.com>
2022-01-18 10:24:15 +0600

    Initial Commit

---
+++ courier/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  courier
+//
+//  Created by Владимир Свиридов on 18.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
